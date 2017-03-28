0b651427c0b79c9d799fc815c0ca8a2a2d89e6d7
Ilene Gorski and Peter Karellas <peter@>
2017-03-28 09:29:31 -0700

    completer counting project

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
-
-
-    var inputs = $("#sentence").val();
-    var array = inputs.split(",");
-    var threeOrMore = [];
-
-    array.forEach(function(word){
-      if(word.length>=3){
-        threeOrMore.push(word);
-      }
-    });
-
-    $("ul").append("<li>"+ threeOrMore + "</li>");
-// console.log(array)
-
-
-
-
-
     event.preventDefault();
+
+    var max= parseInt($("#maximum").val());
+    var multi= parseInt($("#multiple").val());
+
+    for (var index = multi; index <= max; index += multi) {
+      $("ul").append('<li>' + index + "</li>");
+    };
+
+
   });
 });
